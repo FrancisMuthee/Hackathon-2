32285437a47d1f09e355beda9ddd9dd59613594d
Francis <francisnjaramba2@gmail.com>
2024-04-28 15:49:13 +0300

    small email configuration

--- integral/aries/views.py
+++ integral/aries/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+import os
 
 # Create your views here.
 def index(request):
-    return render (request, 'index.html')
+    if request.method == 'POST':
+        name = request.POST.get('full_name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        data = {
+            'name': name,
+            'email': email,
+            'message': message,
+        }
+
+        # Constructing the email message
+        message_content = f"""
+        New message: {data['message']}
+
+        From: {data['email']}
+        """
+
+        # Sending the email
+        send_mail(
+            'New message from your website', # Subject of the email
+            message_content, # Message body
+            settings.EMAIL_HOST_USER, # From email
+            ['reddmarx01@gmail.com'], # To email
+            fail_silently=False,
+        )
+
+        # Optionally, you can redirect to a success page or show a success message
+        # return HttpResponse('Message sent successfully')
+        return render(request, 'index.html', {'message_sent': True})
+
+    return render(request, 'index.html', {})
+
--- integral/integral/settings.py
+++ integral/integral/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +122,18 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myprojstatic')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'francisnjaramba2@gmail.com'
+EMAIL_PASSWORD = 'ciyn epxi ixnx ccoz'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
