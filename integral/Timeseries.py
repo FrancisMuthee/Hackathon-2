c0fb95a687329247fd04f5980f68f2aec2863eaa
Francis <francisnjaramba2@gmail.com>
2024-05-08 12:41:39 +0300

    plotly and normalization

--- integral/Timeseries.py
+++ integral/Timeseries.py
@@ -4,11 +4,19 @@
 #matplotlib Data visualizatio and graphical representation
 #scikit learn for linear regression
 
+
 from datetime import date
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+from statsmodels.graphics.tsaplots import month_plot
+import scipy.stats
+import pylab
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import autofit
 
 
 from sklearn.linear_model import LinearRegression
@@ -19,6 +27,7 @@ warnings.filterwarnings('ignore')
 Data1= pd.read_csv(r"C:\Users\pc\Desktop\Timeseries\Time Series Forecasting dataset\gold_monthly_csv.csv")
 Data1 = pd.DataFrame(Data1)
 print(Data1.head())
+#print(Data1.describe(Data1['Price']))
 #       Date  Price
 # 0  1950-01  34.73
 # 1  1950-02  34.73
@@ -27,7 +36,7 @@ print(Data1.head())
 # 4  1950-05  34.73
 
 print(Data1.shape) #847 rows, 2columns
-print(Data1.isna().sum())
+print(Data1.isna().sum())#huge database #print(Data1.price.isna().sum())
 print(Data1.isnull().any().any()) #Returns a boolean statement
 
 # kasaa = pd.read_csv(r"C:\Users\pc\Desktop\Timeseries\kasaa monthly totals.csv", index_col='Date')
@@ -40,6 +49,18 @@ Data1['month'] = date
 Data1.drop('Date', axis=1, inplace=True)
 Data1 = Data1.set_index('month')
 print(Data1.head())
+
+# Data1.plot()
+# plt.title("Prices over years")
+# plt.show()
+
+#QQ plot
+# fit = scipy.stats.probplot(Data1.Price, plot=pylab) 
+# pylab.show()
+
+
+
+
 #           
 # Date          Records
 # 1/1/2018       26
@@ -76,10 +97,10 @@ print(Data1.head())
 
 
 
-def EDA(Data1):
-    print(Data1.describe())
+# def EDA(Data1):
+#     print(Data1.describe())
 
-EDA(Data1)
+# EDA(Data1)
 #Results
 #              Price
 # count   847.000000
@@ -90,20 +111,35 @@ EDA(Data1)
 # 50%     319.622000
 # 75%     447.029000
 # max    1840.807000
-def boxplot1(Data1):
-    Data1.index = pd.to_datetime(Data1.index)
-    plt.figure(figsize=(30, 8))
-    sns.boxplot(x=Data1.index.year, y=Data1.Price, data=Data1)
-    plt.title("Monthly gold prices")
-    plt.xlabel("Years")
-    plt.ylabel("Price")
-    plt.xticks(rotation=90)
-    plt.grid()
-    plt.show()
+# def boxplot1(Data1):
+#     Data1.index = pd.to_datetime(Data1.index)
+#     plt.figure(figsize=(30, 8))
+#     sns.boxplot(x=Data1.index.year, y=Data1.Price, data=Data1)
+#     plt.title("Monthly gold prices")
+#     plt.xlabel("Years")
+#     plt.ylabel("Price")
+#     plt.xticks(rotation=90)
+#     plt.grid()
+#     plt.show()
 
-boxplot1(Data1)
+# boxplot1(Data1)
 
 
+# def monthplot(Data1):
+#      # Extract the year and month from the index and create new columns
+#     # Data1['Year'] = Data1.index.year
+#     # Data1['Month'] = Data1.index.month
+    
+#     # Plot the data
+#     # sns.lineplot(x='Month', y='Price', hue='Year', data=Data1)
+#     month_plot(Data1)
+#     plt.title("Monthly gold prices")
+#     plt.xlabel("Years")
+#     plt.ylabel("Price")
+#     plt.grid()
+#     plt.show()
+
+# monthplot(Data1)
 #KASARANI MODELLING
 
 # def kasaaplot(kasaa):
@@ -116,6 +152,80 @@ boxplot1(Data1)
 #     plt.show()
 
 # kasaaplot(kasaa)
+
+
+
+#Augmented Dickey Fuller test
+
+
+#Simplified data
+# import pandas as pd
+
+# def simplify_data(data):
+#     # Convert 'Date' to datetime and extract month, then set it as index
+#     data['Date'] = pd.to_datetime(data['Date'])
+#     data.set_index(data['Date'].dt.month, inplace=True)
+#     # Optionally, drop the original 'Date' column if it's no longer needed
+#     # data.drop(columns=['Date'], inplace=True)
+#     return data
+
+# # Assuming Data1 is your DataFrame
+# # Data1 = simplify_data(Data1)
+# # Data1.head() # To display the first few rows
+
+
+
+# def normalization(series):
+#     avg, dev = Data1['Price'].mean(), Data1.Price.std()
+#     print(avg, dev)
+#     normalized_series = (Data1['Price'] - avg) / dev
+#     result = adfuller(Data1['Price'])
+#     print('ADF Statistic:', result[0])
+#     print('p-value:', result[1])
+#     print('Critical Values:')
+#     for key, value in result[4].items():
+#         print(f'   {key}: {value}')
+#     plt.plot(normalized_series)
+#     plt.show() # Display the plot
+
+# Example usage
+# Assuming Data1 is a pandas Series or DataFrame column you want to normalize
+# normalization(Data1['Price'])
+
+# ADF Statistic: 0.8143240077835305
+# p-value: 0.9918639010465632
+# Critical Values:
+#    1%: -3.4382057088878644
+#    5%: -2.865007578546518
+#    10%: -2.5686164240381513
+
+# mul_decompose = seasonal_decompose(Data1['Price'], model='multiplicative', extrapolate_trend='freq')
+# add_decompose = seasonal_decompose(Data1['Price'], model='additive', extrapolate_trend='freq')
+# # mul_decompose.plot()
+# # plt.show()
+# add_decompose.plot()
+# plt.show()
+
+diff_ts = Data1['Price'].diff().dropna()
+# print(diff_ts)
+# diff_ts.plot()
+plot_pacf(diff_ts, lags=10)
+plt.show()
+# plt.show()
+# result2 = adfuller(diff_ts)
+# print('ADF Statistic:', result2[0])
+# print('p-value:', result2[1])
+# print('Critical Values:')
+# for key, value in result2[4].items():
+#     print(f'   {key}: {value}')
+# plot_pacf(diff_ts)
+# plt.show()
+
+
+
+
+
+
 
 
 
---
+++ integral/kasaa.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller
+import seaborn as sns
+from matplotlib import pylot as plt
+from statsmodels.tsa.arima_process import ArmaProcess
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+
